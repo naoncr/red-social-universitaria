3c9ce1bbe807783507503ccb013869e05b70373e
naoncr <naoncr@gmail.com>
2024-11-24 21:03:46 -0500

    nuevo commit :(

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,17 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
 import { provideRouter } from '@angular/router';
 import { routes } from './app/app.routes';
-
+import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
+import { provideAuth, getAuth } from '@angular/fire/auth';
+import { provideFirestore, getFirestore } from '@angular/fire/firestore';
+import { provideStorage, getStorage } from '@angular/fire/storage';
+import { environment } from './app/environment/environment';
 bootstrapApplication(AppComponent, {
   providers: [
-    provideRouter(routes),  // Nueva forma de proporcionar las rutas
+    provideRouter(routes),
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideAuth(() => getAuth()), // Configuración correcta de Auth
+    provideFirestore(() => getFirestore()),
+    provideStorage(() => getStorage()),
   ],
 });
