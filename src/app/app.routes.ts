73d18420ee1a6fe41f8a949cfd3e1c088a6b545d
naoncr <naoncr@gmail.com>
2024-12-06 23:07:56 -0500

    Componente grupo, evento y feed agregado

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,15 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './paginas/home/home.component';
 import { PerfilComponent } from './paginas/perfil/perfil.component';
+import { FeedComponent } from './paginas/feed/feed.component';
+import { EventoComponent } from './paginas/evento/evento.component';
+import { GrupoComponent } from './paginas/grupo/grupo.component';
 
 export const routes: Routes = [
 
-    {path: "", component: HomeComponent},
+    {path: "", component: FeedComponent},
     {path: "perfil", component: PerfilComponent},
+    {path: "eventos", component: EventoComponent},
+    {path: "grupos", component: GrupoComponent},
 
 ];
